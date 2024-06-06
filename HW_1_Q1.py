ae7de20d029fcdda043a8e8cb7ac792e763ff33b
Chungzter <thisemaildoesnotexitatall@test.com>
2024-06-06 18:34:45 +0330

    Finished Question No.1
    modified:   HW_1_Q1.py

--- HW_1_Q1.py
+++ HW_1_Q1.py
@@ -20,6 +20,18 @@ cir.I   (1,cir.gnd,1 , 5@u_A)
 cir.I   (2,cir.gnd,3 , 13@u_A)
 
 cir.V   (1,2,cir.gnd , 20@u_V)
-cir.V   (1,2,3       , 5@u_V)
+cir.V   (2,2,3       , 5@u_V)
 
 cir.VCVS(1, cir.gnd, 5, 3, cir.gnd, 8)
+# End Netlist
+
+sim      = cir.simulator(temperature=25,nominal_temperature=25)
+analysis = sim.operating_point()
+
+print("\nNode Voltages   : ")
+for node in analysis.nodes.values(): 
+    print('Node {}: {:5.2f} V'.format(str(node), float(node[0])))
+
+print("\nBranch Currents : ")
+for node in analysis.branches.values(): 
+    print('Node {}: {:5.2f} A'.format(str(node), float(node[0])))
