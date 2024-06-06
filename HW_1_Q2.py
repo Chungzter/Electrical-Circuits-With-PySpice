e476f5fb7d6b8bd086b7bd3a8720dc90fcb9d579
Chungzter <thisemaildoesnotexitatall@test.com>
2024-06-06 19:42:14 +0330

    Q2: fixed CCVS error while executing the code.
    There should be a branch specification for the curcuit.
    Why? Because there might be more than one branch between two nodes.
    Because of that we add another element:
    Vtest with 0 Volts, allowing us to name the branch we want to measure.

--- HW_1_Q2.py
+++ HW_1_Q2.py
@@ -8,20 +8,25 @@ import PySpice.Logging.Logging as Logging
 ################################################
 logger=Logging.setup_logging()
 
-cir = Circuit("1st_Question")
+cir = Circuit("2nd_Question")
 
 # Netlist
 cir.R   (1,3,cir.gnd , 1@u_Ohm)
 cir.R   (2,1,cir.gnd , 2@u_Ohm)
 cir.R   (3, 2, 3     , 2@u_Ohm)
-cir.R   (4, 3, 1     , 3@u_Ohm)
+cir.R   (4, 3, 4     , 3@u_Ohm)
 
 cir.I   (1,cir.gnd,2 , 10@u_A)
 cir.I   (2, 1, 3     , 5@u_A)
-
 cir.V   (1, 2, 3     , 10@u_V)
 
-cir.CCVS(1, cir.gnd, 2, 3, 1,       2)
+cir.V   ('test',4,1  , 0@u_V)
+# We need this supply to associate CCVS.
+
+cir.CCVS(1, cir.gnd, 2, 'Vtest'        , 2)
+# Name Vtest is used instead of (3,1), because there are two 
+# elements between these nodes and the currents are different
+
 cir.VCCS(1, cir.gnd, 1, 3, cir.gnd, 2)
 # End Netlist
 
