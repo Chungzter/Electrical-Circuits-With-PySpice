46d126466e7a624fafea0e287f7c4b0bd2e8d75c
Chungzter <thisemaildoesnotexitatall@test.com>
2024-06-06 19:11:20 +0330

    Added Netlist to Q2.
    modified:   HW_1_Q2.py

--- HW_1_Q2.py
+++ HW_1_Q2.py
@@ -10,3 +10,17 @@ logger=Logging.setup_logging()
 
 cir = Circuit("1st_Question")
 
+# Netlist
+cir.R   (1,3,cir.gnd , 1@u_Ohm)
+cir.R   (2,1,cir.gnd , 2@u_Ohm)
+cir.R   (3, 2, 3     , 2@u_Ohm)
+cir.R   (4, 3, 1     , 3@u_Ohm)
+
+cir.I   (1,cir.gnd,2 , 10@u_A)
+cir.I   (2, 1, 3     , 5@u_A)
+
+cir.V   (1, 2, 3     , 10@u_V)
+
+cir.CCVS(1, cir.gnd, 2, 3, 1,       2)
+cir.VCCS(1, cir.gnd, 1, 3, cir.gnd, 2)
+# End Netlist
