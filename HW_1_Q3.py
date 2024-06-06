93becdb610662ca9f17d4ba1782fbd77283d80e3
Chungzter <thisemaildoesnotexitatall@test.com>
2024-06-06 20:22:14 +0330

    Q3: Added Netlist.
    This Netlist is a little bit different: First we define some variables, then we use them.

--- HW_1_Q3.py
+++ HW_1_Q3.py
@@ -8,4 +8,22 @@ import PySpice.Logging.Logging as Logging
 ################################################
 logger=Logging.setup_logging()
 
-cir = Circuit("2nd_Question")
+cir = Circuit("3rd_Question")
+
+# Netlist
+r = 5
+a = 1
+b = cir.gnd
+c = 4
+d = 3 
+
+cir.V   (1, a, 2     , 15@u_V)
+cir.V   (2, c, d     , 12@u_V)
+cir.I   (1,cir.gnd,2 , 75@u_A)
+cir.I   (2,d, cir.gnd,  6@u_A)
+cir.I   (3,cir.gnd,c , 10@u_A)
+
+cir.CCVS(1, 2, d, d, 2      , r)
+cir.CCVS(2, 2, cir.gnd, d, 2, 6)
+cir.VCCS(1, 1, cir.gnd, c, d, 9)
+# End Netlist
