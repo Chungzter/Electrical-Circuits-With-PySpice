2b6ca5707e9212c762196919ac38c64844a443ac
Chungzter <thisemaildoesnotexitatall@test.com>
2024-06-06 19:57:13 +0330

    Q3: Created the file, Added Boiler Plate Code.
    new file:   HW_1_Q3.py

---
+++ HW_1_Q3.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import PySpice
+from PySpice.Spice.Netlist import Circuit
+from PySpice.Unit import *
+import PySpice.Logging.Logging as Logging
+################################################
+logger=Logging.setup_logging()
+
+cir = Circuit("2nd_Question")
